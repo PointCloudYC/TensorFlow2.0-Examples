550fcc8402dee01aeefb86a3caf8c92465cea2a8
PointCloudYC <cyinac@connect.ust.hk>
2021-01-28 15:58:07 +0800

    review unet

--- 5-Image_Segmentation/Unet/Unet.py
+++ 5-Image_Segmentation/Unet/Unet.py
@@ -15,7 +15,16 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import Conv2D, Input, MaxPooling2D, Dropout, concatenate, UpSampling2D
 
+"""
+(H,W,1）--> (28,28,2024)
+encoder: [conv1(1,2)]-pool1-[conv2(1,2)]-pool2-...-[conv5(1,2)] 
+P.S>: dropouts are added for conv4 and conv5; conv1(1,2) denote two consecutive conv layers
 
+(28,28,2024) --> (H,W,K)
+decoder: [transConv(6), concat, conv6(1,2)]-[transConv(7), concat, conv7(1,2)]-...[transConv(9), concat, conv9(1,2,3,4)]
+
+Note: this implementation has minor problem, e.g. some conv layers' padding should be valid.
+"""
 def Unet(num_class, image_size):
 
     inputs = Input(shape=[image_size, image_size, 1])
@@ -37,8 +46,9 @@ def Unet(num_class, image_size):
     conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same')(conv5)
     drop5 = Dropout(0.5)(conv5)
 
+    # transponse conv2d (upsampl+conv2d)
     up6 = Conv2D(512, 2, activation = 'relu', padding = 'same')(UpSampling2D(size = (2,2))(drop5))
-    merge6 = concatenate([drop4,up6], axis = 3)
+    merge6 = concatenate([drop4,up6], axis = 3) # concat here not addition
     conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same')(merge6)
     conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same')(conv6)
 
@@ -57,7 +67,8 @@ def Unet(num_class, image_size):
     conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same')(merge9)
     conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same')(conv9)
     conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same')(conv9)
-    conv10 = Conv2D(num_class, 1, activation = 'sigmoid')(conv9)
+    conv10 = Conv2D(num_class, 1, activation = 'sigmoid')(conv9) # 1x1 conv
+
     model = Model(inputs = inputs, outputs = conv10)
     model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
 
